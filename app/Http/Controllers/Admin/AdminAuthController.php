db28f530df5e70e255bc8c3b6a1a4c7900b63b07
apachertr180navneeet <navneet@brenin.co>
2026-08-01 17:02:06 +0530

    feat: implement AdminAuthController to handle authentication, password management, and profile updates

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
